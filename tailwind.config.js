d27210210ff0eedb34d950a5ebe8d3dc732d3fe9
Sharjun Hussain <Sharjoonibnuhussain99@gmail.com>
2024-11-29 20:19:21 +0530

    add shadcn components

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,17 @@ module.exports = {
     			'chart-2': 'hsl(var(--chart-2))',
     			'chart-3': 'hsl(var(--chart-3))',
     			'chart-4': 'hsl(var(--chart-4))',
-    			'chart-5': 'hsl(var(--chart-5))'
+    			'chart-5': 'hsl(var(--chart-5))',
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
     		},
     		borderRadius: {
     			lg: 'var(--radius)',
